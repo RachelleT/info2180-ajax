46af94fb06335e77392a69fcb0b69d1429982117
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2017-10-05 21:35:51 -0500

    updated homepage for no refresh example

--- home.php
+++ home.php
@@ -1,2 +1,26 @@
 <h2>Home</h2>
 <p>This is my home page.</p>
+
+<div class="card-grid">
+    <div class="card">
+        <img src="http://placehold.it/300x200" alt="">
+        <div class="card-body">
+            <h3>Heading</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta porro, praesentium perferendis itaque ex soluta quo impedit dignissimos exercitationem iure voluptates cum optio iste at commodi quasi dolore reiciendis sequi!</p>
+        </div>
+    </div>
+    <div class="card">
+        <img src="http://placehold.it/300x200" alt="">
+        <div class="card-body">
+            <h3>Heading</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta porro, praesentium perferendis itaque ex soluta quo impedit dignissimos exercitationem iure voluptates cum optio iste at commodi quasi dolore reiciendis sequi!</p>
+        </div>
+    </div>
+    <div class="card">
+        <img src="http://placehold.it/300x200" alt="">
+        <div class="card-body">
+            <h3>Heading</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta porro, praesentium perferendis itaque ex soluta quo impedit dignissimos exercitationem iure voluptates cum optio iste at commodi quasi dolore reiciendis sequi!</p>
+        </div>
+    </div>
+</div>
